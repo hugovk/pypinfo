6f6eef83769a9379c3f7bf86bfeabf5ec37af6c4
Hugo <hugovk@users.noreply.github.com>
2018-08-27 18:05:36 +0300

    Add option to show previously saved JSON

--- pypinfo/cli.py
+++ pypinfo/cli.py
@@ -10,6 +10,7 @@ from pypinfo.core import (
     create_client,
     create_config,
     format_json,
+    load_json_from_file,
     parse_query_result,
     tabulate,
 )
@@ -73,6 +74,7 @@ TO_CENTS = Decimal('0.00')
 @click.option('--auth', '-a', help='Path to Google credentials JSON file.')
 @click.option('--run/--test', default=True, help='--test simply prints the query.')
 @click.option('--json', '-j', is_flag=True, help='Print data as JSON, with keys `rows` and `query`.')
+@click.option('--load-json', '-lj', help='Show data from this JSON file.')
 @click.option('--indent', '-i', type=int, help='JSON indentation level.')
 @click.option('--timeout', '-t', type=int, default=120000, help='Milliseconds. Default: 120000 (2 minutes)')
 @click.option('--limit', '-l', help='Maximum number of query results. Default: 10')
@@ -93,6 +95,7 @@ def pypinfo(
     auth,
     run,
     json,
+    load_json,
     indent,
     timeout,
     limit,
@@ -115,57 +118,66 @@ def pypinfo(
         click.echo('Credentials location set to "{}".'.format(get_credentials()))
         return
 
-    if project is None and not fields:
-        click.echo(ctx.get_help())
-        return
+    if load_json:
+        query_info, rows = load_json_from_file(load_json)
+        bytes_billed = query_info['bytes_billed']
+        bytes_processed = query_info['bytes_processed']
+        estimated_cost = query_info['estimated_cost']
+        from_cache = query_info['cached']
+    else:
+        if project is None and not fields:
+            click.echo(ctx.get_help())
+            return
 
-    parsed_fields = []
-    for field in fields:
-        parsed = FIELD_MAP.get(field)
-        if parsed is None:
-            raise ValueError('"{}" is an unsupported field.'.format(field))
-        parsed_fields.append(parsed)
+        parsed_fields = []
+        for field in fields:
+            parsed = FIELD_MAP.get(field)
+            if parsed is None:
+                raise ValueError('"{}" is an unsupported field.'.format(field))
+            parsed_fields.append(parsed)
 
-    order_name = order
-    order = FIELD_MAP.get(order)
-    if order:
-        order_name = order.name
-        parsed_fields.insert(0, order)
+        order_name = order
+        order = FIELD_MAP.get(order)
+        if order:
+            order_name = order.name
+            parsed_fields.insert(0, order)
 
-    built_query = build_query(
-        project,
-        parsed_fields,
-        limit=limit,
-        days=days,
-        start_date=start_date,
-        end_date=end_date,
-        where=where,
-        order=order_name,
-        pip=not all_installers,
-    )
+        built_query = build_query(
+            project,
+            parsed_fields,
+            limit=limit,
+            days=days,
+            start_date=start_date,
+            end_date=end_date,
+            where=where,
+            order=order_name,
+            pip=not all_installers,
+        )
 
     if run:
-        client = create_client(get_credentials())
-        query_job = client.query(built_query, job_config=create_config())
-        query_rows = query_job.result(timeout=timeout // 1000)
+        if not load_json:
+            client = create_client(get_credentials())
+            query_job = client.query(built_query, job_config=create_config())
+            query_rows = query_job.result(timeout=timeout // 1000)
 
-        # Cached
-        from_cache = not not query_job.cache_hit
+            # Cached
+            from_cache = not not query_job.cache_hit
 
-        # Processed
-        bytes_processed = query_job.total_bytes_processed or 0
+            # Processed
+            bytes_processed = query_job.total_bytes_processed or 0
+
+            # Billed
+            bytes_billed = query_job.total_bytes_billed or 0
+
+            # Cost
+            billing_tier = query_job.billing_tier or 1
+            estimated_cost = Decimal(TIER_COST * billing_tier) / TB * Decimal(bytes_billed)
+            estimated_cost = str(estimated_cost.quantize(TO_CENTS, rounding=ROUND_UP))
+
+            rows = parse_query_result(query_job, query_rows)
+
         processed_amount, processed_unit = convert_units(bytes_processed)
-
-        # Billed
-        bytes_billed = query_job.total_bytes_billed or 0
         billed_amount, billed_unit = convert_units(bytes_billed)
-
-        # Cost
-        billing_tier = query_job.billing_tier or 1
-        estimated_cost = Decimal(TIER_COST * billing_tier) / TB * Decimal(bytes_billed)
-        estimated_cost = str(estimated_cost.quantize(TO_CENTS, rounding=ROUND_UP))
-
-        rows = parse_query_result(query_job, query_rows)
 
         if percent:
             rows = add_percentages(rows, include_sign=not json)
